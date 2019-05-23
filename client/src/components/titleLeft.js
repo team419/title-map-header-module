e5b955d5138ef036bb0e7008545ec0cd4e7d41f7
Daniel Kim <nautilus@Nautilus.local>
2019-05-23 15:45:23 -0700

    Add svg icon support for details modal button

--- client/src/components/titleLeft.js
+++ client/src/components/titleLeft.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import iconClaimed from '../images/icon-claimed.png';
+import ratingFourStar from '../images/rating-four-star.png';
 
 const TitleLeft = (props) => {
   const { business } = props;
@@ -14,10 +15,19 @@ const TitleLeft = (props) => {
       </div>
 
       <div className="title-left-entry" id="left-entry-1">
-        stars, reviws, modal
+        <img alt="rating" id="img-rating" src={ratingFourStar} />
+        <span id="review-count">487 reviews</span>
+        <button type="button" id="btn-details-modal">
+          <svg role="img" className="icon-svg" id="svg-histogram" viewBox="0 0 14 14">
+            <path d="M 9 11 V 5 h 2 v 6 H 9 Z M 6 3 h 2 v 8 H 6 V 3 Z M 3 7 h 2 v 4 H 3 V 7 Z" />
+          </svg>
+          <span>Details</span>
+        </button>
       </div>
 
-      <div className="title-left-entry" id="left-entry-2">price, type, edit</div>
+      <div className="title-left-entry" id="left-entry-2">
+        price, type, edit
+      </div>
     </div>
   );
 };
